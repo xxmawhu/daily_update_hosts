76cfa169d9b9205a67ff0e2358d7c35fd8d31f7d
Ma Xin-Xin <xxmawhu@163.com>
2024-04-29 23:14:37 +0800

    update

--- update_daily.sh
+++ update_daily.sh
@@ -1,6 +1,7 @@
 current_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 cd $current_dir
 exec >>./log/$(basename ${BASH_SOURCE[0]}).$(date +"%Y%m%d" && mkdir -p log) 2>&1
+git pull --rebase
 bash get_least_host.sh
 python ./update_hosts.py
 sudo cp ./sys_hosts /etc/hosts
