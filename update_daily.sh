52e41698d45da8e7c8ba6cd4ab0b38b91a9b8d13
Ma Xin-Xin <xxmawhu@163.com>
2024-05-11 10:08:04 +0800

    update

--- update_daily.sh
+++ update_daily.sh
@@ -1,6 +1,7 @@
 current_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 cd $current_dir
 exec >>./log/$(basename ${BASH_SOURCE[0]}).$(date +"%Y%m%d" && mkdir -p log) 2>&1
+find ./log/ -type f -mtime +7 -exec rm -rf {} +
 git pull --rebase
 bash get_least_host.sh
 python ./update_hosts.py
