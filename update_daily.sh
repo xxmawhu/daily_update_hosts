c43f97e07e440ecfb0ad03728a3e10227bb7c7fb
Ma Xin-Xin <xxmawhu@163.com>
2024-05-11 10:27:23 +0800

    update

--- update_daily.sh
+++ update_daily.sh
@@ -5,5 +5,5 @@ source /opt/script/tool.sh
 checkout_silently find ./log/ -type f -mtime +7 -exec rm -rf {} +
 checkout_silently git pull --rebase
 checkout_silently wget https://hosts.gitcdn.top/hosts.txt -O hosts
-checkout_silently /miniconda3/bin/python ./update_hosts.py
+checkout_silently python3 update_hosts.py
 checkout_silently cp ./sys_hosts /etc/hosts
