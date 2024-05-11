dc7213588d07bdef28a757591fd85ca3c9db1860
Ma Xin-Xin <xxmawhu@163.com>
2024-05-11 10:11:28 +0800

    update

--- update_daily.sh
+++ update_daily.sh
@@ -4,5 +4,5 @@ exec >>./log/$(basename ${BASH_SOURCE[0]}).$(date +"%Y%m%d" && mkdir -p log) 2>&
 find ./log/ -type f -mtime +7 -exec rm -rf {} +
 git pull --rebase
 bash get_least_host.sh
-python ./update_hosts.py
+/miniconda3/bin/python ./update_hosts.py
 sudo cp ./sys_hosts /etc/hosts
