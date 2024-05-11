4b6ca05b5685ed121bf9d904d53bcf608be4fd9b
Ma Xin-Xin <xxmawhu@163.com>
2024-05-11 10:25:17 +0800

    update

--- get_least_host.sh
+++
@@ -1,9 +0,0 @@
-
-
-wget https://hosts.gitcdn.top/hosts.txt -O hosts >>/dev/null 2>&1
-if [[ $? -ne 0 ]]; then
-    echo "$(date) | ERROR download hosts fail!"
-    exit 1
-else
-    echo "$(date) | download hosts success"
-fi
