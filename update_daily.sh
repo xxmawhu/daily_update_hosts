a302e9b4f82d00b2c67b84a761334e7f6050fc62
Ma Xin-Xin <xxmawhu@163.com>
2024-05-22 14:09:41 +0800

    update

--- update_daily.sh
+++ update_daily.sh
@@ -1,8 +1,9 @@
+set -e
 current_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 cd $current_dir
 exec >>./log/$(basename ${BASH_SOURCE[0]}).$(date +"%Y%m%d" && mkdir -p log) 2>&1
 source tool.sh
-checkout_silently python3 add_cront.py
+#checkout_silently python3 add_cront.py
 checkout_silently find ./log/ -type f -mtime +7 -exec rm -rf {} +
 checkout_silently git pull --rebase
 checkout_silently wget https://hosts.gitcdn.top/hosts.txt -O hosts
